59b2f8f6e9aae05926f42dd706c9c062c5f6636d
ariyn <himnowxz@gmail.com>
2023-10-09 20:38:58 +0900

    안쓰는 파일들 삭제

--- packet/util.go
+++ packet/util.go
@@ -30,35 +30,3 @@ func Sizeof(t reflect.Value) int {
 
 	return -1
 }
-
-//func Sizeof(t reflect.Type) int {
-//	switch t.Kind() {
-//	case reflect.Array:
-//		//fmt.Println("reflect.Array")
-//		if s := Sizeof(t.Elem()); s >= 0 {
-//			return s * t.Len()
-//		}
-//
-//	case reflect.Struct:
-//		//fmt.Println("reflect.Struct")
-//		sum := 0
-//		for i, n := 0, t.NumField(); i < n; i++ {
-//			s := Sizeof(t.Field(i).Type)
-//			if s < 0 {
-//				return -1
-//			}
-//			sum += s
-//		}
-//		return sum
-//
-//	case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
-//		reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
-//		reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128:
-//		return int(t.Size())
-//	case reflect.Slice:
-//		return 0
-//	}
-//
-//	return -1
-//
-//}
